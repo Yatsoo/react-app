bbcaf486d16705812a144b14dd342bd9ded59786
Yatsoo <Yatsoo@users.noreply.github.com>
2020-12-22 00:04:50 +0100

    updated Search Engine

--- src/Weather.js
+++ src/Weather.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
-import FormattedDate from "./FormattedDate";
+
 import WeatherInfo from "./WeatherInfo";
 import axios from "axios";
 import "./weather.css";
 
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
 
   function handleResponse(response) {
     setWeatherData({
@@ -20,11 +21,26 @@ export default function Weather(props) {
     });
   }
 
+  function search() {
+    const apiKey = "5641bc2ed9f7604fde1dadce23d1af77";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
   if (weatherData.ready) {
     return (
       <div className="currentdayweather">
         <div className="searchform">
-          <form className="form-inline">
+          <form className="form-inline" onSubmit={handleSubmit}>
             <div className="row">
               <div className="form-group mx-sm-3 mb-2">
                 <input
@@ -33,6 +49,7 @@ export default function Weather(props) {
                   placeholder="Enter a City"
                   autoComplete="off"
                   autoFocus="on"
+                  onChange={handleCityChange}
                 />
               </div>
               <span className="searchbutton">
@@ -47,146 +64,11 @@ export default function Weather(props) {
             </div>
           </form>
         </div>
-        <WeatherInfo />
-        <div className="card-deck">
-          <div className="card text-left" style={{ maxWidth: "18rem" }}>
-            <div className="card-header">
-              <h1>
-                <span>{weatherData.city}</span>
-              </h1>
-            </div>
-            <div className="card-body">
-              <div className="row">
-                <div className="col">
-                  <FormattedDate date={weatherData.date} />
-                </div>
-                <div className="col">
-                  <h5>
-                    <span>{Math.round(weatherData.temperature)}°C</span>
-                  </h5>
-                  <small>
-                    <a href="/" className="active">
-                      C{" "}
-                    </a>{" "}
-                    | <a href="/">F</a>
-                  </small>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className="card text-right">
-            <div className="card-body">
-              <div className="extrainfo text-left">
-                <ul>
-                  <li>
-                    <img
-                      src={weatherData.icon}
-                      className="card-img"
-                      alt={weatherData.description}
-                    />
-                  </li>
-                  <li>
-                    <br />
-                  </li>
-                  <li>
-                    <h6>
-                      <span className="text-capitalize">
-                        {weatherData.description}
-                      </span>
-                    </h6>
-                  </li>
-                  <li>
-                    <hr className="line" />
-                  </li>
-                  <li>
-                    <h6>
-                      Humidity: <span>{weatherData.humidity}</span>%{" "}
-                    </h6>
-                  </li>
-                  <li>
-                    <h6>
-                      Wind: <span>{weatherData.wind}</span> km/h
-                    </h6>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <hr />
-
-        <div className="weeklytemperature">
-          <div className="row">
-            <div className="col d-flex justify-content-center">
-              <div className="card text-center">
-                <div className="card-header">Saturday</div>
-                <img
-                  src="https://img.icons8.com/nolan/64/smiling-sun.png"
-                  className="card-img-top"
-                  alt="sun shining"
-                />
-                <div className="card-footer">23°C</div>
-              </div>
-            </div>
-
-            <div className="col d-flex justify-content-center">
-              <div className="card text-center">
-                <div className="card-header">Sunday</div>
-                <img
-                  src="https://img.icons8.com/nolan/64/partly-cloudy-day.png"
-                  className="card-img-top"
-                  alt="cloud sun"
-                />
-                <div className="card-footer">19°C</div>
-              </div>
-            </div>
-
-            <div className="col d-flex justify-content-center">
-              <div className="card text-center">
-                <div className="card-header">Monday</div>
-                <img
-                  src="https://img.icons8.com/nolan/64/clouds.png"
-                  className="card-img-top"
-                  alt="clouds"
-                />
-                <div className="card-footer">21°C</div>
-              </div>
-            </div>
-
-            <div className="col d-flex justify-content-center">
-              <div className="card text-center">
-                <div className="card-header">Tuesday</div>
-                <img
-                  src="https://img.icons8.com/nolan/64/partly-cloudy-day.png"
-                  className="card-img-top"
-                  alt="partly cloudy"
-                />
-                <div className="card-footer">17°C</div>
-              </div>
-            </div>
-
-            <div className="col d-flex justify-content-center">
-              <div className="card text-center">
-                <div className="card-header">Wednesday</div>
-                <img
-                  src="https://img.icons8.com/nolan/64/rain.png"
-                  className="card-img-top"
-                  alt="rainy"
-                />
-                <div className="card-footer">21°C</div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <WeatherInfo data={weatherData} />
       </div>
     );
   } else {
-    const apiKey = "5641bc2ed9f7604fde1dadce23d1af77";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
-
+    search();
     return "Loading...";
   }
 }
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,141 @@
 import React from "react";
+import FormattedDate from "./FormattedDate";
 
-export default function WeatherInfo() {
-  return "Weather info";
+export default function WeatherInfo(props) {
+  return (
+    <div className="WeatherInfo">
+      <div className="card-deck">
+        <div className="card text-left" style={{ maxWidth: "18rem" }}>
+          <div className="card-header">
+            <h1>
+              <span>{props.data.city}</span>
+            </h1>
+          </div>
+          <div className="card-body">
+            <div className="row">
+              <div className="col">
+                <FormattedDate date={props.data.date} />
+              </div>
+              <div className="col">
+                <h5>
+                  <span>{Math.round(props.data.temperature)}°C</span>
+                </h5>
+                <small>
+                  <a href="/" className="active">
+                    C{" "}
+                  </a>{" "}
+                  | <a href="/">F</a>
+                </small>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="card text-right">
+          <div className="card-body">
+            <div className="extrainfo text-left">
+              <ul>
+                <li>
+                  <img
+                    src={props.data.icon}
+                    className="card-img"
+                    alt={props.data.description}
+                  />
+                </li>
+                <li>
+                  <br />
+                </li>
+                <li>
+                  <h6>
+                    <span className="text-capitalize">
+                      {props.data.description}
+                    </span>
+                  </h6>
+                </li>
+                <li>
+                  <hr className="line" />
+                </li>
+                <li>
+                  <h6>
+                    Humidity: <span>{props.data.humidity}</span>%{" "}
+                  </h6>
+                </li>
+                <li>
+                  <h6>
+                    Wind: <span>{props.data.wind}</span> km/h
+                  </h6>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <hr />
+
+      <div className="weeklytemperature">
+        <div className="row">
+          <div className="col d-flex justify-content-center">
+            <div className="card text-center">
+              <div className="card-header">Saturday</div>
+              <img
+                src="https://img.icons8.com/nolan/64/smiling-sun.png"
+                className="card-img-top"
+                alt="sun shining"
+              />
+              <div className="card-footer">23°C</div>
+            </div>
+          </div>
+
+          <div className="col d-flex justify-content-center">
+            <div className="card text-center">
+              <div className="card-header">Sunday</div>
+              <img
+                src="https://img.icons8.com/nolan/64/partly-cloudy-day.png"
+                className="card-img-top"
+                alt="cloud sun"
+              />
+              <div className="card-footer">19°C</div>
+            </div>
+          </div>
+
+          <div className="col d-flex justify-content-center">
+            <div className="card text-center">
+              <div className="card-header">Monday</div>
+              <img
+                src="https://img.icons8.com/nolan/64/clouds.png"
+                className="card-img-top"
+                alt="clouds"
+              />
+              <div className="card-footer">21°C</div>
+            </div>
+          </div>
+
+          <div className="col d-flex justify-content-center">
+            <div className="card text-center">
+              <div className="card-header">Tuesday</div>
+              <img
+                src="https://img.icons8.com/nolan/64/partly-cloudy-day.png"
+                className="card-img-top"
+                alt="partly cloudy"
+              />
+              <div className="card-footer">17°C</div>
+            </div>
+          </div>
+
+          <div className="col d-flex justify-content-center">
+            <div className="card text-center">
+              <div className="card-header">Wednesday</div>
+              <img
+                src="https://img.icons8.com/nolan/64/rain.png"
+                className="card-img-top"
+                alt="rainy"
+              />
+              <div className="card-footer">21°C</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
