0a7855dd4d71b95db520fcee32d361e9c9b7d6fb
Yatsoo <Yatsoo@users.noreply.github.com>
2020-12-22 01:09:55 +0100

    update icon Url

--- src/Weather.js
+++ src/Weather.js
@@ -14,7 +14,7 @@ export default function Weather(props) {
       city: response.data.name,
       date: new Date(response.data.dt * 1000),
       temperature: response.data.main.temp,
-      icon: "https://img.icons8.com/nolan/64/rain.png",
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
       description: response.data.weather[0].description,
       humidity: response.data.main.humidity,
       wind: response.data.wind.speed,
